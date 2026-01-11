ca28f289d7f273b2cd40779961df6da250f8fd8e
Alvaro Ferraz <alvaro.ferrazdcruz@gmail.com>
2026-01-11 13:30:57 -0300

    update

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import './src/services/api';
 import './src/config/reactotronConfig';
 import './src/config/axiosConfig';
 import './src/config/navigationConfig';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Teste - Se isso aparecer, o problema está em outra tela</Text>
-    </View>
-  );
+  return <AppNavigator />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+const styles = StyleSheet.create({});
